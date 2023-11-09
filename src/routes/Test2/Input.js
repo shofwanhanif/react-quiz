41dc893c403d72eaa22a45170469fa8910bed9de
shofwanhanif <shofwan.hanif@tokopedia.com>
2023-11-09 13:24:04 +0700

    feat: Add solution number 2

--- src/routes/Test2/Input.js
+++ src/routes/Test2/Input.js
@@ -1,5 +1,7 @@
-const Input = () => {
-  return <input type="text" placeholder="input here" />
+const Input = (props) => {
+  const { value, setValue } = props
+  
+  return <input type="text" placeholder="input here" value={value} onChange={(e) => setValue(e.target.value)}  />
 }
 
 export default Input;
