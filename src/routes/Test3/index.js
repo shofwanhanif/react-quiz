6ef865987981aa62afaeff5a5f5ce7ee7a3e1ed1
shofwanhanif <shofwan.hanif@tokopedia.com>
2023-11-09 13:24:18 +0700

    feat: Add solution number 4

--- src/routes/Test3/index.js
+++ src/routes/Test3/index.js
@@ -1,13 +1,21 @@
+import { useRef } from 'react'
+
 const Test3 = () => {
+  const fieldRef = useRef(null)
+
+  const handleClick = () => {
+    fieldRef.current.focus()
+  }
+
   return(
     <div>
       <ul>
         <li>Please focus to the text field when user click the focus button</li>
       </ul>
       <p>
-        <button type="button">Focus Text Field</button>
+        <button type="button" onClick={handleClick} >Focus Text Field</button>
       </p>
-      <input type="text" placeholder="focus me"/>
+      <input type="text" placeholder="focus me" ref={fieldRef} />
     </div>
   )
 }
