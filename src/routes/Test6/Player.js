ac7ee78284cb5a5ee848c0e264298f3d8432fafb
shofwanhanif <shofwan.hanif@tokopedia.com>
2023-11-15 11:06:38 +0700

    feat: Add solution number 6

--- src/routes/Test6/Player.js
+++ src/routes/Test6/Player.js
@@ -1,16 +1,53 @@
 import { cssVideo } from "./style";
 
+import { useState, useRef } from 'react'
+
 const Player = () => {
+  const videoRef = useRef(null)
+  const [play, setPlay] = useState(false)
+  const [mute, setMute] = useState(false)
+  const [currentTimeVideo, setCurrentTimeVideo] = useState(0)
+  const [durationVideo, setDurationVideo] = useState(0)
+  const [seekTime, setSeekTime] = useState(0)
+
+  const handlePlay = () => {
+    if (!play) videoRef.current.play()
+    else videoRef.current.pause()
+    
+    setPlay(!play)
+  }
+
+  const handleVideoTime = () => {
+    setCurrentTimeVideo(videoRef.current.currentTime)
+    setDurationVideo(videoRef.current.duration)
+    setSeekTime(videoRef.current.currentTime)
+  }
+
+  const handleChangeSeek = (time) => {
+    videoRef.current.currentTime = time
+    setSeekTime(time)
+  }
+
+  const handleFormatTime = (time) => {
+    const minutes = Math.floor(time / 60)
+    const second = Math.floor(time % 60)
+    const resultTime = `${('0'+ minutes).slice(-2)}:${('0'+ second).slice(-2)}` 
+    return resultTime
+  }
+  
   return(
     <div>
       <video
         className={cssVideo}
         src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        ref={videoRef}
+        muted={mute}
+        onTimeUpdate={handleVideoTime}
       />
-      <button type="button">Play/Pause</button>
-      <button type="button">Mute/Unmute</button>
-      <input type="range"/>
-      <span>00:00/00:00</span>
+      <button type="button" onClick={handlePlay}>{!play ? 'Play' : 'Pause'}</button>
+      <button type="button" onClick={() => setMute(!mute)}>{!mute ? 'Mute' : 'Unmute'}</button>
+      <input type="range" value={seekTime} min='0' max={durationVideo} step='0.01' onChange={(e) => handleChangeSeek(e.target.value)} />
+      <span>{handleFormatTime(currentTimeVideo)}/{handleFormatTime(durationVideo)}</span>
     </div>
   )
 }
