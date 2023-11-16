f1ceed1de63ca19e41ccd836bb2fe70a8f55329c
shofwanhanif <shofwan.hanif@tokopedia.com>
2023-11-16 08:27:37 +0700

    feat: Add solutions number 9

--- src/routes/Test9/index.js
+++ src/routes/Test9/index.js
@@ -1,6 +1,19 @@
 import Solar from "./Solar";
+import { useState } from "react";
 
 const Test9 = () => {
+  const [value, setValue] = useState(0)
+
+  const handleChangeValue = (data) => {
+    const regex = /^[0-9\b]+$/; 
+
+    if (parseInt(data) > 9)
+      return
+
+    if (data === '' || regex.test(data))
+      setValue(data)
+  }
+
   return (
     <>
       <ul>
@@ -9,8 +22,8 @@ const Test9 = () => {
         <li>The color of each planet must be unique</li>
         <li>More far the planet = more time it's revolves</li>
       </ul>
-      <input type="text" placeholder="number of planet"/>
-      <Solar/>
+      <input type="text" placeholder="number of planet" value={value} onChange={(e) => handleChangeValue(e.target.value)}/>
+      <Solar value={value}/>
     </>
   )
 }
--- src/routes/Test9/style.js
+++ src/routes/Test9/style.js
@@ -1,13 +1,14 @@
-import { css } from 'react-emotion';
+import { css, keyframes } from 'react-emotion';
 
 export const cssSpace = css({
   marginTop: 24,
-  width: 800,
-  height: 800,
+  width: 1000,
+  height: 1000,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
   backgroundColor: 'black',
+  position: 'relative'
 });
 
 export const cssSun = css({
@@ -17,9 +18,28 @@ export const cssSun = css({
   borderRadius: 40,
 });
 
-export const cssPlanet = () => css({
+const keyframesSpin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  };
+`
+
+export const cssOrbit = (animationTime) => css({
+  top: '50%',
+  right: '50%',
+  position: 'absolute',
+  animation: `${keyframesSpin} ${animationTime}s linear infinite`
+});
+
+export const cssPlanet = (bgColor, topPosition) => css({
   width: 20,
   height: 20,
   borderRadius: 10,
-  backgroundColor: 'red',
+  backgroundColor: bgColor,
+  position: 'absolute',
+  top: topPosition
 });
